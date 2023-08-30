af5863ebce81bfc87d8c246b69c5dc51b5d747d5
Datamayor <106524048+Datamayor@users.noreply.github.com>
2023-08-30 16:23:13 +0100

    lets go api

--- main.js
+++ main.js
@@ -29,3 +29,35 @@ function scrollr() {
     var RightMove = document.querySelector(".lets-go-courasel");
     RightMove.scrollBy(350, 0)
 }
+
+const newsFeedDiv = document.getElementById('newsFeed');
+
+// Replace 'YOUR_API_KEY' with your actual API key
+const apiKey = 'b1ceb852918642249d76c1de446fce9f';
+const apiUrl = `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${apiKey}`;
+// Fetch news data from the News API
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    const articles = data.articles.slice(0, 3); // Only take the top 3 articles
+
+    // Process the news data and update the newsFeedDiv content
+    const newsHTML = articles.map(article => `
+      <div class="news-item">
+        <div class="news-image">
+          <img src="${article.urlToImage}" alt="News Image">
+        </div>
+        <div class="news-content">
+          <h2>${article.title}</h2>
+          <p>${article.description}</p>
+          <a href="${article.url}" target="_blank">Read more</a>
+        </div>
+      </div>
+    `).join('');
+
+    // Update the content of the newsFeedDiv
+    newsFeedDiv.innerHTML = newsHTML;
+  })
+  .catch(error => {
+    console.error('Error fetching news data:', error);
+  });
