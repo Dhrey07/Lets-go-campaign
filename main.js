d07134f0180502584cc44ce15c964943d9469daf
Datamayor <106524048+Datamayor@users.noreply.github.com>
2023-08-30 19:22:06 +0100

    good

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ const newsContainer = document.querySelector('.news-container');
 
 // Replace 'YOUR_API_KEY' with your actual API key
 const apiKey = 'b1ceb852918642249d76c1de446fce9f';
-const apiUrl = `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${apiKey}`;
+const apiUrl = `https://newsapi.org/v2/everything?q=tesla&from=2023-07-30&sortBy=publishedAt&apiKey=b1ceb852918642249d76c1de446fce9f`;
 
 let newsData = [];
 let currentSlide = 0;
